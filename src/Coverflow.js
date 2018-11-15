b381bdac08842f4ef831edf593413ac4cd0b9e6d
Alex Ellsworth <alex.ells21@gmail.com>
2018-11-15 16:23:38 -0700

    changes to window

--- src/Coverflow.js
+++ src/Coverflow.js
@@ -86,11 +86,13 @@ class Coverflow extends Component {
       }
     });
 
-    // const eventListener = window && window.addEventListener;
+    if (window) {
+      const eventListener = window && window.addEventListener;
 
-    // if (eventListener) {
-    //   window.addEventListener('resize', this.updateDimensions.bind(this));
-    // }
+      if (eventListener) {
+        window.addEventListener("resize", this.updateDimensions.bind(this));
+      }
+    }
   }
 
   componentWillReceiveProps(nextProps) {
@@ -263,7 +265,9 @@ class Coverflow extends Component {
       // If on the active figure
       if (typeof action === "string") {
         // If action is a URL (string), follow the link
-        // window.open(action, '_blank');
+        if (window) {
+          window.open(action, "_blank");
+        }
       }
 
       this._removePointerEvents();
